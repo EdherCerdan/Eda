52830b1d1e984dd3174fe7807c604737d0749fae
EdherCerdan <josimar_93_12@hotmail.com>
2020-05-17 22:05:59 -0500

    Update Tree.cpp
    Nueva Modificacion

--- Tree.cpp
+++ Tree.cpp
@@ -1,394 +1,266 @@
-#include<iostream>
-#include<queue>
-#include<stack>
+#include <iostream> 
+#include <vector>
+#include <queue>
 #include <algorithm>
-using namespace std;
- 
+#include <stack>
+#include <math.h>
+#include <string>
+using namespace std; 
 
-class BTreeNode
-{
-    int *keys;  
-    int t;      
-    BTreeNode **C;
-    int n;    
-    bool leaf;
+class Node{
 public:
-    BTreeNode(int _t, bool _leaf);  
- 
-    void insertNonFull(int k);
-     
-    void splitChild(int i, BTreeNode *y);
-   
-    void traverse();
-   
-    BTreeNode *search(int k);  
- 
-
-
-friend class BTree;
+    int data;
+    Node *left, *right;
+    Node(int val){
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
 };
- 
 
-class BTree
-{
-    BTreeNode *root;
-    int t;  
-public:
-   
-    BTree(int _t)
-    {  root = NULL;  t = _t; }
+class BST 
+{ 
+    public: 
+    Node *root;
+    BST(int val){
+        root = new Node(val);
+    }
+    vector<int> search_path(int val){
+        vector<int> path;
+        Node *temp = root;
+        while(temp != nullptr and temp->data!=val){
+            path.push_back(temp->data);
+            if (val < temp->data)
+                temp = temp->left;
+            else
+                temp = temp->right;
+        }
+        if (temp != nullptr)
+            path.push_back(temp->data);
+        else
+            path.clear();
+        return path;
+    }
 
-    void inorder()
-    {  if (root != NULL) root->traverse(); }
+    void insert(int key) 
+    { 
+        Node* newnode = new Node(key); 
+        Node* x = root; 
+        Node* y = NULL;   
+        while (x != NULL) { 
+            y = x; 
+            if (key < x->data) 
+                x = x->left; 
+            else
+                x = x->right; 
+        } 
+        if (y == NULL) 
+            y = newnode; 
+        else if (key < y->data) 
+            y->left = newnode; 
+        else
+            y->right = newnode; 
+    } 
+    void inorder(Node *n, vector<int> &vec){
+        if(n != 0){
+            inorder(n->left, vec); 
+            vec.push_back(n->data);
+            inorder(n->right, vec);
+        }
+    }
+    void BFS(vector<int> &vec){  
+        queue<Node *> q; 
+        q.push(root);   
+        while (q.empty() == false){ 
+            Node *node = q.front();
+            vec.push_back(node->data);
+            q.pop(); 
+            if (node->left != 0){
+                q.push(node->left);
+                
+            }
+            if (node->right != 0){
+                q.push(node->right);
+            }
+        }
+        cout<<endl;
+    }
+    void DFS(vector<int> &vec){
+        vector<Node*> visited;
+        stack<Node*> to_visit;
+        to_visit.push(root);
+        while(!to_visit.empty()){
+            Node *curr_node = to_visit.top();
+            to_visit.pop();
+            if (find(visited.begin(), visited.end(), curr_node ) == visited.end()){
+                vec.push_back(curr_node->data);
+                visited.push_back(curr_node);
+                
+            }
+            
+            if (find(visited.begin(), visited.end(), curr_node->right ) == visited.end()){
+                if (curr_node->right){
+                    to_visit.push(curr_node->right);
+                }
+            }
 
-    BTreeNode* search(int k)
-    {  return (root == NULL)? NULL : root->search(k); }
+            if (find(visited.begin(), visited.end(), curr_node->left ) == visited.end()){
+                if (curr_node->left){
+                    to_visit.push(curr_node->left);
+                }
+            }
+    
+        }
+    }
+    void veb(Node *t, vector<int> &vec){
+        // veb from top tree
+        if(t and t->left and t->right){
+            vec.push_back(t->data);
+            vec.push_back(t->left->data);
+            vec.push_back(t->right->data);
+            veb(t->left, vec);
+            veb(t->right, vec);
+        }
 
-    void insert(int k);
-    void BFS();
-    void DFS();
-    void veb();
+    }
+    int search_blocks(vector<int> layout, int blocks, int val){
+        int total_blocks = layout.size()/blocks;
+        vector<int> path = search_path(val);
+        if (path.size() == 0){
+            cout<<"elemento no encontrado en arbol"<<endl;
+            return -1;
+        }
+
+        cout<<"camino: "<<endl;
+        for(int i = 0; i < path.size(); i++){
+            cout<<path[i]<<" ";
+        }
+        cout<<endl;
+
+        cout<<"recorrido: "<<endl;
+        for(int i = 0; i < layout.size(); i++){
+            if(i%blocks == 0 and i!=0)
+                cout<<"- ";
+            cout<<layout[i]<<" ";
+        }
+        cout<<endl;
+        
+        int n_blocks=0;
+        int i = 0; // pos recorrido
+        int j = 0; // post busqueda
+        bool block_has_element = false;
+        bool new_block = false;
+        while(i < layout.size() and j < path.size()){
+            if (i%blocks == 0){
+                new_block = true;
+                block_has_element = false;
+            }
+            if (layout[i] == path[j]){
+                j += 1;
+                block_has_element = true;
+            }
+            if (block_has_element and new_block){
+                n_blocks += 1;
+                new_block = false;
+            }
+            i+=1;
+
+        }
+        cout<<"numero bloques a recorrer: "<<n_blocks<<endl;
+        return n_blocks;
+    }
 };
- 
-// Constructor for BTreeNode class
-BTreeNode::BTreeNode(int t1, bool leaf1)
-{
-    // Copy the given minimum degree and leaf property
-    t = t1;
-    leaf = leaf1;
- 
-    // Allocate memory for maximum number of possible keys
-    // and child pointers
-    keys = new int[2*t-1];
-    C = new BTreeNode *[2*t];
- 
-    // Initialize the number of keys as 0
-    n = 0;
-}
- 
-void BTreeNode::traverse()
-{
-    int i;
-    for (i = 0; i < n; i++)
-    {
-        // cout<<"n: "<<n<<endl;
-        if (leaf == false) {
-            // cout<<endl;
-            C[i]->traverse();
-        }
-        cout<<" "<<keys[i];
-    }
-    if (leaf == false) {
-        cout<<endl;
-        C[i]->traverse();
-    }
-}
- 
+  
+// Driver code 
+int main() 
+{ 
+    BST tree(8);
+    tree.insert(4);
+    tree.insert(12);
+    tree.insert(2);
+    tree.insert(6);
+    tree.insert(1);
+    tree.insert(3);
+    tree.insert(5);
+    tree.insert(7);
+    tree.insert(10);
+    tree.insert(14);
+    tree.insert(9);
+    tree.insert(11);
+    tree.insert(13);
+    tree.insert(15);
+    
+    vector<int> vec_inorder;
+    vector<int> vec_BFS;
+    vector<int> vec_DFS;
+    vector<int> vec_veb;
 
-BTreeNode *BTreeNode::search(int k)
-{
-   
-    int i = 0;
-    while (i < n && k > keys[i])
-        i++;
- 
-   
-    if (keys[i] == k)
-        return this;
- 
-   
-    if (leaf == true)
-        return NULL;
- 
-   
-    return C[i]->search(k);
-}
- 
+    tree.inorder(tree.root, vec_inorder);
+    tree.BFS(vec_BFS);
+    tree.DFS(vec_DFS);
 
-void BTree::insert(int k)
-{
-   
-    if (root == NULL)
-    {
-       
-        root = new BTreeNode(t, true);
-        root->keys[0] = k;  
-        root->n = 1;  
-    }
-    else
-    {
-       
-        if (root->n == 2*t-1)
-        {
-           
-            BTreeNode *s = new BTreeNode(t, false);
-           
-            s->C[0] = root;
- 
-            s->splitChild(0, root);      
-           
-            int i = 0;
-            if (s->keys[0] < k)
-                i++;
-            s->C[i]->insertNonFull(k);
- 
-           
-            root = s;
-        }
-        else  
-            root->insertNonFull(k);
-    }
-}
- 
-
-
-
-void BTreeNode::insertNonFull(int k)
-{
-   
-    int i = n-1;
- 
-   
-    if (leaf == true)
-    {
-       
-       
-       
-        while (i >= 0 && keys[i] > k)
-        {
-            keys[i+1] = keys[i];
-            i--;
-        }
- 
-       
-        keys[i+1] = k;
-        n = n+1;
-    }
-    else
-    {
-       
-        while (i >= 0 && keys[i] > k)
-            i--;
- 
-       
-        if (C[i+1]->n == 2*t-1)
-        {
-           
-            splitChild(i+1, C[i+1]);
- 
-           
-           
-           
-            if (keys[i+1] < k)
-                i++;
-        }
-        C[i+1]->insertNonFull(k);
-    }
-}
- 
-
-
-void BTreeNode::splitChild(int i, BTreeNode *y)
-{
-   
-   
-    BTreeNode *z = new BTreeNode(y->t, y->leaf);
-    z->n = t - 1;
- 
-   
-    for (int j = 0; j < t-1; j++)
-        z->keys[j] = y->keys[j+t];
- 
-   
-    if (y->leaf == false)
-    {
-        for (int j = 0; j < t; j++)
-            z->C[j] = y->C[j+t];
-    }
- 
-   
-    y->n = t - 1;
- 
-   
-   
-    for (int j = n; j >= i+1; j--)
-        C[j+1] = C[j];
- 
-   
-    C[i+1] = z;
- 
-   
-   
-    for (int j = n-1; j >= i; j--)
-        keys[j+1] = keys[j];
- 
-   
-    keys[i] = y->keys[t-1];
- 
-   
-    n = n + 1;
-}
- 
-void BTree::BFS(){
-   
-    if (root == NULL)  return;
- 
-   
-    queue<BTreeNode *> q;
- 
-   
-    q.push(root);
- 
-    while (q.empty() == false)
-    {
-       
-        BTreeNode *node = q.front();
-       
-       
-       
-        for(int i = 0; i < node->n; i++){
-            cout << node->keys[i]<<"-";
-        }
-        q.pop();
-        cout<<endl;
-        if (node->leaf == false){
-            for(int i = 0; i < node->n +1; i++){
-                q.push(node->C[i]);
-            }
-        }
-    }
-}
-
-void BTree::DFS(){
-vector<BTreeNode*> visited;
-
-stack<BTreeNode*> to_visit;
-to_visit.push(root);
-while(!to_visit.empty()){
-BTreeNode *curr_node = to_visit.top();
-to_visit.pop();
-if (find(visited.begin(), visited.end(), curr_node ) != visited.end()){
-
-}
-else{
-for(int i = 0; i < curr_node->n;i++){
-cout<<curr_node->keys[i]<<" ";
-}
-cout<<endl;
-visited.push_back(curr_node);
-
-}
-
-for(int i = curr_node->n + 1 ; i > -1;i--){
-if (find(visited.begin(), visited.end(), curr_node->C[i] ) != visited.end()){
-
-}
-else{
-if (curr_node->C[i]){
-
-
-to_visit.push(curr_node->C[i]);
-}
-}
-}
-}
-}
-
-void BTree::veb(){
-int h = 0;
-BTreeNode *temp = root;
-while(temp != 0){
-h+=1;
-
-temp = temp->C[0];
-
-
-}
-
-temp = root;
-queue<BTreeNode*> q;
-vector<BTreeNode*> previous_level;
-    q.push(root);
-    int counter = 0;
-    int total_nodes = 1;
-    int prev_level = 1;
-    for(int i = 1; i < h/2 + 1; i++){
-   
-    total_nodes += i*(2*root->t - 1);
-    }
-    for(int i = 1; i < h/2; i++){
-   
-    prev_level += i*(2*root->t - 1);
-    }
-    while (counter < total_nodes)
-    {
-        BTreeNode *node = q.front();
-        if (counter < prev_level){
-        for(int i = 0; i < node->n; i++){
-           cout << node->keys[i]<<"-";
-        }
-        cout<<endl;
-        }
-        q.pop();
-        if (node->leaf == false){
-            for(int i = 0; i < node->n +1; i++){
-                q.push(node->C[i]);
-            }
-        }
-        if (counter >=prev_level){
-        previous_level.push_back(node);
-        }
-    counter += 1;
+    cout<<"inorder: "<<endl;
+    for(int i = 0; i < vec_inorder.size(); i++){
+        cout<<vec_inorder[i]<<" ";
     }
     cout<<endl;
-    for(int i = 0; i < previous_level.size(); i++){
-   
-    queue<BTreeNode*> q2;
-    q2.push(previous_level[i]);
-    while (!q2.empty())
-    {
-        BTreeNode *node2 = q2.front();
-        for(int i = 0; i < node2->n; i++){
-           cout << node2->keys[i]<<"-";
+
+    cout<<"BFS: "<<endl;
+    for(int i = 0; i < vec_BFS.size(); i++){
+        cout<<vec_BFS[i]<<" ";
+    }
+    cout<<endl;
+
+    cout<<"DFS: "<<endl;
+    for(int i = 0; i < vec_DFS.size(); i++){
+        cout<<vec_DFS[i]<<" ";
+    }
+    cout<<endl;
+
+    vec_veb.push_back(8);
+    vec_veb.push_back(4);
+    vec_veb.push_back(12);
+    vec_veb.push_back(2);
+    vec_veb.push_back(1);
+    vec_veb.push_back(3);
+    vec_veb.push_back(6);
+    vec_veb.push_back(5);
+    vec_veb.push_back(7);
+    vec_veb.push_back(10);
+    vec_veb.push_back(9);
+    vec_veb.push_back(11);
+    vec_veb.push_back(14);
+    vec_veb.push_back(13);
+    vec_veb.push_back(15);
+    cout<<"vam Ende Boas: "<<endl;
+    for(int i = 0; i < vec_veb.size(); i++){
+        cout<<vec_veb[i]<<" ";
+    }
+    cout<<endl;
+
+    while(true){
+        int option;
+
+        cout<<"ingresa recorrido a usar (inorder: 1, dfs:2 , bfs:3, van emde boas:4)"<<endl;
+        cin>>option;
+
+        int b, e;
+        cout<<"ingresa tam bloque: ";cin>>b;
+        // cout<<endl;
+        cout<<"ingresa elemento a buscar: ";cin>>e;
+        // cout<<endl;
+        if(option == 1){
+            tree.search_blocks(vec_inorder, b, e);
         }
-        q2.pop();
-        cout<<endl;
-        if (node2->leaf == false){
-           for(int i = 0; i < node2->n +1; i++){
-                q2.push(node2->C[i]);
-            }
+        if(option == 2){
+            tree.search_blocks(vec_DFS, b, e);
+        }
+        if(option == 3){
+            tree.search_blocks(vec_BFS, b, e);
+        }
+        if(option == 4){
+            tree.search_blocks(vec_veb, b, e);
         }
     }
-    }
-}
-
-int main()
-{
-    BTree t(2);
-    t.insert(10);
-    t.insert(20);
-    t.insert(30);
-    t.insert(40);
-    t.insert(50);
-    t.insert(60);
-    t.insert(70);
-    t.insert(80);
-    t.insert(90);
-    t.insert(100);
-    t.insert(110);
-    t.insert(120);
-    t.insert(130);
-    t.insert(140);
-    t.insert(150);
-
-   
-    t.inorder();
-    cout<<endl;
-    t.BFS();
-    cout<<endl;
-    t.DFS();
-    cout<<endl;
-    t.veb();
- 
-    return 0;
+    return 0; 
 } 
